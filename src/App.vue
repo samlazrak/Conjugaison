bf27c1837b663ff4926f390b10b967c1d873ab26
Sam Lazrak <Lazrak13@uab.edu>
2019-10-18 08:18:44 -0500

    veutify

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,37 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Welcome to Conjugaison" />
-    <Conjugaison></Conjugaison>
-  </div>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld />
+    </v-content>
+  </v-app>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import Vue from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
-import Conjugaison from "./components/Conjugaison.vue";
 
-@Component({
+export default Vue.extend({
+  name: "App",
   components: {
-    HelloWorld,
-    Conjugaison
-  }
-})
-export default class App extends Vue {}
+    HelloWorld
+  },
+  data: () => ({
+    //
+  })
+});
 </script>
-
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
